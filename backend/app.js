73b84d8930e46e12190c5ad6e83fcd556e714ce4
Juho Salonoja <juhid@hotmail.com>
2021-11-23 05:08:33 +0200

    Setting up RESTapi

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+
+// Setting up config.env variables
+dotenv.config({path: './config/config.env'});
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=> {
+    console.log(`Server started on port ${process.env.PORT}`)
+});
