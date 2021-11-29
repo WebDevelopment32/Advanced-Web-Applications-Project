09de95280981e7b74ec56e0b2083ea4df5d137a4
Juho Salonoja <juhid@hotmail.com>
2021-11-29 22:17:05 +0200

    Added global error handling methods for Async, Unhandled Promise Rejection, Uncaught Exeptions, validation, mongose ID and unhandled routes. Modified gitignore to ignole npm installs.

--- backend/controllers/restaurantsController.js
+++ backend/controllers/restaurantsController.js
@@ -1,8 +1,11 @@
 const restaurant = require('../models/restaurant');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 // Export this getRestaurants method to router.
 // Get all restaurants => /api/v1/restaurants
-exports.getRestaurants = async (req, res, next) => {
+// Wrapping the async method inside catchAsyncErrors so that if error occures, it gets handles by the CAE method
+exports.getRestaurants = catchAsyncErrors(async (req, res, next) => {
     const restaurants = await restaurant.find();
 
     // HTTP status code 200 = OK "The request has succeeded"
@@ -11,4 +14,9 @@ exports.getRestaurants = async (req, res, next) => {
         results: restaurants.length,
         data: restaurants
     });
-};
+});
+
+// Use this error handling method in methods for single resourse if sertain resourse is not found
+if(!restaurant || restaurant.length === 0) {
+    return next(new ErrorHandler('Restaurant not found', 404));
+}
---
+++ backend/middlewares/errors.js
@@ -0,0 +1,39 @@
+const ErrorHandler = require('../utils/errorHandler');
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+
+    if(process.env.NODE_ENV === 'development') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack: err.stack
+        });
+    }
+
+    if(process.env.NODE_ENV === 'production') {
+        let error = {...err};
+        error.message = err.message;
+
+        // Wrong mongoose Object ID Error
+        // Occurs when resourse is not found (Wrong ID for item given in req)
+        if(err.name === 'CastError') {
+            const message = `Resource not found. Invalid ${err.path}`;
+            error = new ErrorHandler(message, 404);
+        }
+
+        // Handling Mongoose Validation Error. Combining
+        // Occurs when more than one or more resourses is not given in a req for schema
+        // Related to Async error
+        if(err.name === 'ValidationError') {
+            const message = Object.values(err.errors.map(value => value.message));
+            error = new ErrorHandler(message, 400);
+        }
+
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || 'Internal server error.'
+        });
+    }
+}
